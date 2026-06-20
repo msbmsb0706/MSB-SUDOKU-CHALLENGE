3e7116353d714b026ce49a36004fdf3d36d9ba40
Mahalingam Babu <232755675+msbmsb0706@users.noreply.github.com>
2026-06-20 13:58:10 +0530

    Remove signing config from release build type
    Remove signing configuration from release build type.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
       isCrunchPngs = false
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
-    }
+      
+      }
     debug {
     }
   }
